e21a6b0156764627f3c00eaf51a7aa090d6701c3
yqq <youngqqcn@gmail.com>
2024-03-28 17:04:08 +0800

    update

--- nft_metadata/metadata/gen.py
+++ nft_metadata/metadata/gen.py
@@ -1,7 +1,12 @@
+#coding:utf8
+import json
+
+
+template = """
 {
     "description": "Fansland is the first redefines music festival experience with innovative Web3.0 technology, art and music will be landed in Bangkok 2024. We're All Gonna Make It.",
     "image": "ipfs://bafybeieawzz6q3u7lqbnodctnknirquqo3j2iejeju3w544lksloa4jcgy",
-    "name": "Early Bird",
+    "name": "Early Bird ",
     "attributes": [
         {
             "trait_type": "Event Name",
@@ -25,3 +30,19 @@
         }
     ]
 }
+"""
+
+
+def main():
+    for i in range(0, 1000):
+
+        s = json.loads(template)
+        s['name'] = "Early Bird #{}".format(i)
+        with open(f'./early_bird_1000/{i}', 'w') as ofile:
+            json.dump(fp=ofile, obj=s ,indent=4)
+    pass
+
+
+if __name__ == '__main__':
+
+    main()
